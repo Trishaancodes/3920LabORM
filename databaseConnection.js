8803e60556b40290b50baa7662a2b993b13611e5
Trishaan <tshetty1@my.bcit.ca>
2026-03-23 17:28:55 -0700

    switched db

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 
 const dbConfigLocal = {
 	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	multipleStatements: false,
-	namedPlaceholders: true
+	namedPlaceholders: true,
+	ssl: {
+		rejectUnauthorized: false
+	}
 };
 
 const database = mysql.createPool(dbConfigLocal);
